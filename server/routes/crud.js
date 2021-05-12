0c2df85348cfed47970450a3a9e5101750a68f14
Alkin Maystorov <alkin971@gmail.com>
2021-05-12 18:28:43 +0200

    add create startup post route

--- server/routes/crud.js
+++ server/routes/crud.js
@@ -2,24 +2,52 @@ const router = require("express").Router();
 const StartUp = require("../models/StartUp");
 const Investor = require("../models/Investor");
 
-
 router.post("/signup/investor", (req, res, next) => {
-  const {email, username, password, firstName, lastName} = req.body;
-  Investor.create({email, username, password, firstName, lastName})
-          .then(investor => {
-            res.status(200).json(investor);
-          })
-          .catch(err=> next(err))
+  const { email, username, password, firstName, lastName } = req.body;
+  Investor.create({ email, username, password, firstName, lastName })
+    .then((investor) => {
+      res.status(200).json(investor);
+    })
+    .catch((err) => next(err));
 });
 
-router.post("/signUp/startUp", (req, res, next) => {
-  const {email, username, password, firstName, lastName} = req.body;
-  Investor.create({email, username, password, firstName, lastName})
-          .then(investor => {
-            res.status(200).json(investor);
-          })
-          .catch(err=> next(err))
+router.post("/signup/startup", (req, res, next) => {
+  const {
+    companyName,
+    email,
+    username,
+    password,
+    dateRegistered,
+    industry,
+    stage,
+    foundation,
+    teamMembers,
+    skillsAvailable,
+    skillsComplete,
+    skillsNeeded,
+    experience,
+    location,
+  } = req.body;
+  StartUp.create({
+    companyName,
+    email,
+    username,
+    password,
+    dateRegistered,
+    industry,
+    stage,
+    foundation,
+    teamMembers,
+    skillsAvailable,
+    skillsComplete,
+    skillsNeeded,
+    experience,
+    location,
+  })
+    .then((startUp) => {
+      res.status(200).json(startUp);
+    })
+    .catch((err) => next(err));
 });
-
 
 module.exports = router;
