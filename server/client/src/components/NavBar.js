4a704864b8ff3b18b7459806ba323ee7889e5248
helenetrommelen <helenetrommelen@gmail.com>
2021-05-13 11:41:57 +0200

    added startupEvaluation

---
+++ server/client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+      <h1>Hello from NavBar</h1>
+      
+    // <ul>
+    //   <li>
+    //     <Link to="/">Home</Link>
+    //   </li>
+    // </ul>
+  )
+}
