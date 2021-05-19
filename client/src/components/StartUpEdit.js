26b4df59b567e3c1fd487bd07eadacc0614a7c0d
Alkin Maystorov <alkin971@gmail.com>
2021-05-19 19:57:35 +0200

    investor profile done startup wip

--- client/src/components/StartUpEdit.js
+++ client/src/components/StartUpEdit.js
@@ -25,7 +25,7 @@ export default class StartUpEdit extends Component {
       it: false,
     },
     skillsII: this.props.skillsII,
-    skillsIII:this.props.skillsIII,
+    skillsIII: this.props.skillsIII,
     skillsIIIchecked: {
       finance: false,
       marketing: false,
@@ -33,7 +33,6 @@ export default class StartUpEdit extends Component {
       it: false,
     },
     experience: this.props.experience,
-    
   };
 
   getData = () => {
@@ -62,44 +61,42 @@ export default class StartUpEdit extends Component {
       .catch((err) => console.log(err));
   };
 
-  handleChecked = 
-    async (e) => {    
-    const name = e.target.name;    
-    await           
-      this.setState(state => ({
-        [name]: !state[name]   //this works
-        }))
+  handleChecked = async (e) => {
+    const name = e.target.name;
+    await this.setState((state) => ({
+      [name]: !state[name], //this works
+    }));
 
-        const skills=[];
-        for (let key in this.state) {
-        this.state[key] === true && skills.push(key)
-        }   
-    };
+    const skills = [];
+    for (let key in this.state) {
+      this.state[key] === true && skills.push(key);
+    }
+  };
 
-    handleChange = (e) => {
-      const name = e.target.name;    
-      this.setState({
-        [name]: e.target.value  
-        })
-      }
+  handleChange = (e) => {
+    const name = e.target.name;
+    this.setState({
+      [name]: e.target.value,
+    });
+  };
 
   componentDidMount() {
     this.getData();
     console.log("check array: ", this.state.skillsIchecked);
-      for (let key in this.state.skillsIchecked) {
-          if (this.state.skillsI.includes(key)) {
-             this.setState({
-                skillsIchecked: {[key]: true}
-             })
-          }
+    for (let key in this.state.skillsIchecked) {
+      if (this.state.skillsI.includes(key)) {
+        this.setState({
+          skillsIchecked: { [key]: true },
+        });
       }
-      for (let key in this.state.skillsIIIchecked) {
-        if (this.state.skillsIII.includes(key)) {
-           this.setState({
-              skillsIIIchecked: {[key]: true}
-           })
-        }
+    }
+    for (let key in this.state.skillsIIIchecked) {
+      if (this.state.skillsIII.includes(key)) {
+        this.setState({
+          skillsIIIchecked: { [key]: true },
+        });
       }
+    }
   }
 
   render() {
@@ -158,7 +155,9 @@ export default class StartUpEdit extends Component {
           <br />
 
           <label htmlFor="description">Description: </label>
-          <input
+          <textarea
+            rows="10"
+            cols="30"
             type="text"
             id="description"
             name="description"
@@ -206,52 +205,94 @@ export default class StartUpEdit extends Component {
           <br />
 
           {/* STAGE */}
-            <div onChange={this.handleChange} value={this.state.stage} checked={this.state.stage}>
-              <label htmlFor="stage">In what stage is your idea?</label>
+          <div
+            onChange={this.handleChange}
+            value={this.state.stage}
+            checked={this.state.stage}
+          >
+            <label htmlFor="stage">In what stage is your idea?</label>
+            <br />
+            <div class="answerRadio">
+              <label for="prototype">
+                <input
+                  type="radio"
+                  id="Prototype"
+                  name="stage"
+                  value="prototype"
+                  checked={this.state.stage === "prototype"}
+                />
+                Prototype/MVP (Pre-Seed)
+              </label>
               <br />
-              <div class="answerRadio">
-                  <label for="prototype">
-                  <input type="radio" id="Prototype" name="stage" value="prototype" checked={this.state.stage==='prototype'}/>
-                    Prototype/MVP (Pre-Seed)
-                  </label>
-                  <br />
-                  <label for="concept">
-                  <input type="radio" id="Concept" name="stage" value="concept" checked={this.state.stage==='concept'} />
-                    Proof of Concept (Pre-Seed)
-                  </label>
-                  <br />
-                  <label for="paying customers">
-                  <input type="radio" id="Customers" name="stage" value="customers" checked={this.state.stage==='customers'}/>
-                    First Paying Customers (Seed)
-                  </label>
-                  <br />
-                  <label for="beyond">
-                  <input type="radio" id="beyond" name="stage" value="beyond" checked={this.state.stage==='beyond'}/>
-                    beyond the mentioned ones
-                  </label>
-              </div>
+              <label for="concept">
+                <input
+                  type="radio"
+                  id="Concept"
+                  name="stage"
+                  value="concept"
+                  checked={this.state.stage === "concept"}
+                />
+                Proof of Concept (Pre-Seed)
+              </label>
+              <br />
+              <label for="paying customers">
+                <input
+                  type="radio"
+                  id="Customers"
+                  name="stage"
+                  value="customers"
+                  checked={this.state.stage === "customers"}
+                />
+                First Paying Customers (Seed)
+              </label>
+              <br />
+              <label for="beyond">
+                <input
+                  type="radio"
+                  id="beyond"
+                  name="stage"
+                  value="beyond"
+                  checked={this.state.stage === "beyond"}
+                />
+                beyond the mentioned ones
+              </label>
+            </div>
           </div>
           <br />
 
-           {/* FOUNDATION */}
+          {/* FOUNDATION */}
           <div
             onChange={this.handleChange}
             value={this.state.foundation}
             checked={this.state.foundation}
           >
-            <label htmlFor="foundation">Is your company already founded? </label>
+            <label htmlFor="foundation">
+              Is your company already founded?{" "}
+            </label>
             <br />
-            <input type="radio" id="yes" name="foundation" value={true} checked={this.state.foundation}/>
+            <input
+              type="radio"
+              id="yes"
+              name="foundation"
+              value={true}
+              checked={this.state.foundation}
+            />
             <label for="yes">Yes</label>
             <br />
 
-            <input type="radio" id="no" name="foundation" value={true} checked={this.state.foundation}/>
+            <input
+              type="radio"
+              id="no"
+              name="foundation"
+              value={true}
+              checked={this.state.foundation}
+            />
             <label for="no">No</label>
             <br />
           </div>
           <br />
 
-           {/* MEMBERS */}
+          {/* MEMBERS */}
           <div
             onChange={this.handleChange}
             value={this.state.teamMembers}
@@ -261,86 +302,184 @@ export default class StartUpEdit extends Component {
               How many core team members do you have?
             </label>
             <br />
-            <input type="radio" id="1" name="teamMembers" value="1" checked={this.state.teamMembers==='1'}/>
+            <input
+              type="radio"
+              id="1"
+              name="teamMembers"
+              value="1"
+              checked={this.state.teamMembers === "1"}
+            />
             <label for="1">1</label>
             <br />
 
-            <input type="radio" id="2" name="teamMembers" value="2" checked={this.state.teamMembers==='2'}/>
+            <input
+              type="radio"
+              id="2"
+              name="teamMembers"
+              value="2"
+              checked={this.state.teamMembers === "2"}
+            />
             <label for="2">2</label>
             <br />
 
-            <input type="radio" id="3" name="teamMembers" value="3" checked={this.state.teamMembers==='3'}/>
+            <input
+              type="radio"
+              id="3"
+              name="teamMembers"
+              value="3"
+              checked={this.state.teamMembers === "3"}
+            />
             <label for="3">3</label>
             <br />
 
-            <input type="radio" id="4" name="teamMembers" value="4" checked={this.state.teamMembers==='4'}/>
+            <input
+              type="radio"
+              id="4"
+              name="teamMembers"
+              value="4"
+              checked={this.state.teamMembers === "4"}
+            />
             <label for="4">4</label>
             <br />
 
-            <input type="radio" id="5orMore" name="teamMembers" value="5orMore" checked={this.state.teamMembers==='5orMore'}/>
+            <input
+              type="radio"
+              id="5orMore"
+              name="teamMembers"
+              value="5orMore"
+              checked={this.state.teamMembers === "5orMore"}
+            />
             <label for="5orMore">5 or more</label>
             <br />
-           </div>
+          </div>
           <br />
 
-           {/* SKILLSI */}
+          {/* SKILLSI */}
           <div>
-            <h3>Which skills do you have in your team (professional experience of at least 1 year, a Master or Bachelor degree or something similar)?</h3>
-            <input type="checkbox" id="finance" name="finance"  checked={this.state.skillsI.finance} onChange={this.handleChecked}/>
-            <label htmlFor="finance">Finance</label><br/>
+            <h3>
+              Which skills do you have in your team (professional experience of
+              at least 1 year, a Master or Bachelor degree or something
+              similar)?
+            </h3>
+            <input
+              type="checkbox"
+              id="finance"
+              name="finance"
+              checked={this.state.skillsI.finance}
+              onChange={this.handleChecked}
+            />
+            <label htmlFor="finance">Finance</label>
+            <br />
 
-            <input type="checkbox" id="marketing" name="marketing"  checked={this.state.skillsI.marketing} onChange={this.handleChecked}/>
-            <label htmlFor="marketing">Marketing</label><br/>
+            <input
+              type="checkbox"
+              id="marketing"
+              name="marketing"
+              checked={this.state.skillsI.marketing}
+              onChange={this.handleChecked}
+            />
+            <label htmlFor="marketing">Marketing</label>
+            <br />
 
-            <input type="checkbox" id="sales" name="sales" checked={this.state.skillsI.sales} onChange={this.handleChecked}/>
-            <label htmlFor="sales">Sales</label><br/>
+            <input
+              type="checkbox"
+              id="sales"
+              name="sales"
+              checked={this.state.skillsI.sales}
+              onChange={this.handleChecked}
+            />
+            <label htmlFor="sales">Sales</label>
+            <br />
 
-            <input type="checkbox" id="it" name="it" checked={this.state.skillsI.it} onChange={this.handleChecked}/>
+            <input
+              type="checkbox"
+              id="it"
+              name="it"
+              checked={this.state.skillsI.it}
+              onChange={this.handleChecked}
+            />
             <label htmlFor="it">IT/Tech</label>
-            </div>
+          </div>
           <br />
 
           {/* SKILLSII */}
-          <div
-            onChange={this.handleChange}
-            value={this.state.skillsII}
-          >
+          <div onChange={this.handleChange} value={this.state.skillsII}>
             <label htmlFor="skillsII" value={this.state.stage}>
-              Do you have all the necessary skills in your team to develop the idea?
+              Do you have all the necessary skills in your team to develop the
+              idea?
             </label>
             <br />
-            <input type="radio" id="yes" name="skillsII" value="yes" checked={this.state.skillsII==='yes'} />
+            <input
+              type="radio"
+              id="yes"
+              name="skillsII"
+              value="yes"
+              checked={this.state.skillsII === "yes"}
+            />
             <label for="yes">Yes</label>
             <br />
 
-            <input type="radio" id="no" name="skillsII" value="no" checked={this.state.skillsII==='yes'} />
+            <input
+              type="radio"
+              id="no"
+              name="skillsII"
+              value="no"
+              checked={this.state.skillsII === "yes"}
+            />
             <label for="no">No</label>
             <br />
           </div>
 
           <br />
-          
+
           {/* SKILLSIII */}
           <div>
             <label htmlFor="finance" value={this.props.stage}>
               If "No", which skills are you looking for?
             </label>
             <br />
-            <input type="checkbox" id="finance" name="finance"  checked={this.state.skillsIII.finance} onChange={this.handleChecked}/>
-            <label htmlFor="finance">Finance</label><br/>
+            <input
+              type="checkbox"
+              id="finance"
+              name="finance"
+              checked={this.state.skillsIII.finance}
+              onChange={this.handleChecked}
+            />
+            <label htmlFor="finance">Finance</label>
+            <br />
 
-            <input type="checkbox" id="marketing" name="marketing"  checked={this.state.skillsIII.marketing} onChange={this.handleChecked}/>
-            <label htmlFor="marketing">Marketing</label><br/>
+            <input
+              type="checkbox"
+              id="marketing"
+              name="marketing"
+              checked={this.state.skillsIII.marketing}
+              onChange={this.handleChecked}
+            />
+            <label htmlFor="marketing">Marketing</label>
+            <br />
 
-            <input type="checkbox" id="sales" name="sales" checked={this.state.skillsIII.sales} onChange={this.handleChecked}/>
-            <label htmlFor="sales">Sales</label><br/>
+            <input
+              type="checkbox"
+              id="sales"
+              name="sales"
+              checked={this.state.skillsIII.sales}
+              onChange={this.handleChecked}
+            />
+            <label htmlFor="sales">Sales</label>
+            <br />
 
-            <input type="checkbox" id="it" name="it" checked={this.state.skillsIII.it} onChange={this.handleChecked}/>
+            <input
+              type="checkbox"
+              id="it"
+              name="it"
+              checked={this.state.skillsIII.it}
+              onChange={this.handleChecked}
+            />
             <label htmlFor="it">IT/Tech</label>
           </div>
 
           <br />
-          
+
           <div>
             <label htmlFor="experience">Did you found a startup before?</label>
             <br />
