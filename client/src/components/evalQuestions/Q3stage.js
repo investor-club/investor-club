c5ad27c77ac2ff6862ab23fbeaf550220e220397
helenetrommelen <helenetrommelen@gmail.com>
2021-05-19 09:33:13 +0200

    added styling to questionnaire

--- client/src/components/evalQuestions/Q3stage.js
+++ client/src/components/evalQuestions/Q3stage.js
@@ -7,28 +7,29 @@ export default class Q3stage extends React.Component {
 
   render() {
     return (
-      <div
-        onChange={this.handleChange}
-        value={this.props.stage}
-        checked={this.props.stage}
-      >
+      <div onChange={this.handleChange} value={this.props.stage} checked={this.props.stage}>
         <label htmlFor="stage">In what stage is your idea?</label>
         <br />
         <div class="answerRadio">
-          <input type="radio" id="Prototype" name="stage" value="prototype" checked={this.props.stage==='prototype'}/>
-          <label for="prototype">Prototype/MVP (Pre-Seed)</label>
+          <label for="prototype">
+            <input type="radio" id="Prototype" name="stage" value="prototype" checked={this.props.stage==='prototype'}/>
+            Prototype/MVP (Pre-Seed)
+          </label>
           <br />
-
-          <input type="radio" id="Concept" name="stage" value="concept" checked={this.props.stage==='concept'} />
-          <label for="concept">Proof of Concept (Pre-Seed)</label>
+          <label for="concept">
+            <input type="radio" id="Concept" name="stage" value="concept" checked={this.props.stage==='concept'} />
+            Proof of Concept (Pre-Seed)
+          </label>
           <br />
-
-          <input type="radio" id="Customers" name="stage" value="customers" checked={this.props.stage==='customers'}/>
-          <label for="paying customers">First Paying Customers (Seed)</label>
+          <label for="paying customers">
+            <input type="radio" id="Customers" name="stage" value="customers" checked={this.props.stage==='customers'}/>
+            First Paying Customers (Seed)
+          </label>
           <br />
-
-          <input type="radio" id="beyond" name="stage" value="beyond" checked={this.props.stage==='beyond'}/>
-          <label for="beyond">beyond the mentioned ones</label>
+          <label for="beyond">
+            <input type="radio" id="beyond" name="stage" value="beyond" checked={this.props.stage==='beyond'}/>
+            beyond the mentioned ones
+          </label>
         </div>
       </div>
     );
