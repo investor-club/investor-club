4fa7d55e95f60b7ccf5623a2245d4833a05f59ab
Katarina Orlovska <k.orlovska@posteo.de>
2021-07-19 15:12:24 +0200

    the images

--- client/src/components/SignUpChoice.js
+++ client/src/components/SignUpChoice.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import '../public' from '../public'
-import startupImg from "../public/undraw_upgrade_06a0.svg"
+import startupImg from "../public/undraw_upgrade_06a0.svg"; 
 import investorImg from "../public/undraw_revenue_3osh.svg"
 
 export default class SignUpChoice extends Component {
   render() {
     return (
       <div>
-      <div class='purpleBackground'></div>
+      <div className='purpleBackground'></div>
       <div id='signupChoice'>
         <div id='startupChoice'>
          
           <Link to="/signup/startup">
-            <img src={startupImg} alt="startup image"/>
+            <img src={startupImg} alt="startup"/>
           </Link>          
           <h2>
             <Link to="/signup/startup">I'm a Startup</Link>
@@ -22,7 +22,7 @@ export default class SignUpChoice extends Component {
         <div id='investorChoice'>
           
           <Link to="/signup/investor">
-            <img src={investorImg} alt="investor image"/>
+            <img src={investorImg} alt="investor"/>
           </Link>
           <h2>
             <Link to="/signup/investor">I'm an Investor</Link>
